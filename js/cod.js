fa42a175f5b390a8f1fda86b539b6c1fa6f79588
JulioAburto <juliocesaraburtoguido@gmail.com>
2021-04-06 17:29:54 -0600

    Agregar objeto javascript

--- js/cod.js
+++ js/cod.js
@@ -1,3 +1,15 @@
 function saludo() {
 	alert("Hola Alumnos");
 }
+
+let persona = {
+	nombre: "Juan",
+	apellido: "Perez",
+	email: "jperez@gmail.com",
+	edad: 28,
+	nombreCompleto: function () {
+		return this.nombre + " "+  this.apellido;
+	},
+};
+
+console.log(persona.nombreCompleto());
