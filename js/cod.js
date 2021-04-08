153f71f3d06bdac36741c59ccd09bf142cf67b12
JulioAburto <juliocesaraburtoguido@gmail.com>
2021-04-07 18:29:01 -0600

    Añadiendo parrafos e imagenes

--- js/cod.js
+++ js/cod.js
@@ -8,8 +8,13 @@ let persona = {
 	email: "jperez@gmail.com",
 	edad: 28,
 	nombreCompleto: function () {
-		return this.nombre + " " + this.apellido;
+		return this.nombre + " " + this.apellido + " " + this.email + "" + this.edad;
 	},
 };
 
 console.log(persona.nombreCompleto());
+
+function suma(a, b) {
+	let suma = a + b;
+	return suma;
+}
